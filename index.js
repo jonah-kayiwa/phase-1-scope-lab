b3407251ef7d49c3deaddcfa3c2c5ac0087dc09d
Jonah <kayiwa.jonah@student.moringaschool.com>
2023-12-21 11:59:12 +0300

    ##F

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'John';
+function upperCaseCustomerName() {
+  if (customerName) {
+    return customerName.toUpperCase();
+  } else {
+    return 'Customer name not defined';
+  }
+}
+
+function setBestCustomer() {
+  BestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+  BestCustomer = 'still not bob'; 
+}
+
+const leastFavoriteCustomer = 'Alice';
+function changeLeastFavoriteCustomer() {
+  
+  leastFavoriteCustomer = 'Bob';
+}
